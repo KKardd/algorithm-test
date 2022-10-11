e3d818be0d7fa97f3773799d600152437b8a4139
KKardd <108740187+KKardd@users.noreply.github.com>
2022-10-11 16:10:45 +0900

    정수 내림차순으로 배치하기 알고리즘

---
+++ programmers/javascript/level1/정수 내림차순으로 배치하기.js
@@ -0,0 +1,32 @@
+/*
+문제 설명
+함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+
+제한 조건
+n은 1이상 8000000000 이하인 자연수입니다.
+*/
+
+function solution(n) {
+    var answer = "";
+    let arr = n.toString().split("");
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr.length; j++){
+            if(arr[j] < arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    for(let x = 0; x < arr.length; x++){
+        answer += arr[x];
+    }
+    answer = parseInt(answer);
+    return answer;
+}
+
+
+/*
+function solution(n) {
+    return parseInt((n+"").split("").sort().reverse().join(""));
+}*/
