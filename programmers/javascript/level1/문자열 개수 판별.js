35ffe4f338fd636beceb61ed377eef7dfe31cf76
KKardd <wlstjd4013@gmail.com>
2023-02-08 21:49:09 +0900

    문자열 개수 판별 알고리즘

---
+++ programmers/javascript/level1/문자열 개수 판별.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * 문자열 개수를 판별
+ * 
+ * 스킬체크 1단계 문제
+ */
+
+function solution(s) {
+    var answer = 0;
+    for(let i = 0; i < s.length; i++){
+        let X = 1;
+        let X_no = 0;
+        let j = i + 1;
+        let check = s[i];
+        while(X != X_no){
+            if(check === s[j]){
+                X++;
+            }else{
+                X_no++;
+            }
+            i++;
+            j++;
+            if(j === s.length){
+                answer++;
+                return answer;
+            }
+        }
+        answer++;
+    }
+
+    return answer;
+}
