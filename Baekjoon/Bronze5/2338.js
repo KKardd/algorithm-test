3c97d97500cf52de954aec50b054ce99036d56a6
KKardd <wlstjd4013@gmail.com>
2023-06-20 13:49:37 +0900

    remove previous beakjoon file

--- Baekjoon/Bronze5/2338.js
+++
@@ -1,9 +0,0 @@
-/**
- * https://www.acmicpc.net/problem/2338
- */
-
-var fs = require("fs");
-var input = fs.readFileSync("../test.txt").toString().split("\n").map(BigInt);
-var a = input[0];
-var b = input[1];
-console.log(a + b + "\n" + (a - b) + "\n" + a * b);
