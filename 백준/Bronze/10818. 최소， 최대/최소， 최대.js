2d56043f960c5c5af5fb7f370b78f21fd7c52e09
KKardd <108740187+KKardd@users.noreply.github.com>
2023-06-21 02:59:06 +0900

    [Bronze III] Title: 최소, 최대, Time: 456 ms, Memory: 84996 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -1,16 +1,6 @@
 var fs = require("fs");
 var input = fs.readFileSync("/dev/stdin").toString().split("\n");
-
 const count = input[0];
-let min = 1000000;
-let max = -1000000;
+
 const arr = input[1].split(" ");
-for (let i = 0; i < arr.length; i++) {
-    if (max < parseInt(arr[i])) {
-        max = parseInt(arr[i]);
-    }
-    if (min > parseInt(arr[i])) {
-        min = parseInt(arr[i]);
-    }
-}
-console.log(min + " " + max);
+console.log(Math.min(...arr) + " " + Math.max(...arr));
