116a2f3eca3dd2eb6ec75fa549e7283fd69636c9
KKardd <wlstjd4013@gmail.com>
2023-07-02 00:38:55 +0900

    100점짜리 풀이로 변경

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -2,17 +2,33 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-const N = input[0];
-const M = input[1];
+const N = parseInt(input[0]);
+const M = parseInt(input[1]);
 let S = input[2];
 let answer = 0;
-const compareStr = "I" + "OI".repeat(N);
-for (let i = 0; i < M - compareStr.length + 1; i++) {
+let idx = 0;
+let num = 0;
+for (let i = 0; i < M; i++) {
     if (S[i] === "I") {
-        const sliceS = S.slice(i, i + compareStr.length);
-        if (sliceS === compareStr) {
-            answer++;
-        }
+        num++;
+        idx++;
+    } else if (S[i] === "O") {
+        num--;
+        idx++;
+    }
+    if (num > 1) {
+        num = 1;
+        idx = 1;
+        continue;
+    } else if (num < 0) {
+        num = 0;
+        idx = 0;
+        continue;
+    }
+    if (idx === 1 + 2 * N) {
+        idx -= 2;
+        num = 1;
+        answer++;
     }
 }
 console.log(answer);
