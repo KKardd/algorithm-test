5a2097eae4fd3146a3d9ae1f274bea46d61b16fa
KKardd <108740187+KKardd@users.noreply.github.com>
2023-07-29 11:02:59 +0900

    [level 2] Title: 숫자의 표현, Time: 2.26 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = 1;
+    for(let i = 1; i < n / 2; i++){
+        let sum = i;    
+        for(let j = i+1; j < n / 2 + 1; j++){
+            sum += j;
+            if(sum === n){
+                answer++;
+                break;
+            }else if(sum > n){
+                break;
+            }
+        }
+    }
+    return answer;
+}
